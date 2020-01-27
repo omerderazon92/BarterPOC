888042268eb74f24d57c3715469be4ab8717fd7d
Omer Derazon <omerderazon92@gmail.com>
2020-01-27 19:40:03 +0200

    Initial Commit

---
+++ Barter POCTests/Barter_POCTests.swift
@@ -0,0 +1,34 @@
+//
+//  Barter_POCTests.swift
+//  Barter POCTests
+//
+//  Created by Omer Derazon on 27/01/2020.
+//  Copyright © 2020 Omer Derazon. All rights reserved.
+//
+
+import XCTest
+@testable import Barter_POC
+
+class Barter_POCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
